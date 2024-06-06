e622cf37c5328a01516054c03edbf1026f889a1e
Burak/Home <brkthz@gmail.com>
2024-06-07 00:26:44 +0300

    403 cors sorunu login ekranında çözüldü

--- src/main/java/com/example/mechanic_service/config/SecurityConfig.java
+++ src/main/java/com/example/mechanic_service/config/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
 
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/").permitAll()
+                        .requestMatchers("/api/auth/login").permitAll() // login endpoint'ini herkese açık yapar
                         .anyRequest().authenticated())
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
